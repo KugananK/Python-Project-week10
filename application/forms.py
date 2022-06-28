3f439a7016b3cbc3b5ba7a84816ca5000480c782
Kajan <kajankugananthajothy@gmail.com>
2022-06-28 18:13:34 +0000

    initial routes and templates

--- application/forms.py
+++ application/forms.py
@@ -2,13 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, DecimalField, SelectField
 
 class GameForm(FlaskForm):
-    game_name = StringField("Game")
+    game_name = StringField("Game Name")
     genre = StringField("Genre")
-    release_date = IntegerField("Date")
-    price = DecimalField("Price")
-    publisher = SelectField("Publisher")
+    release_date = IntegerField("Release Date")
+    price = DecimalField("Price on release")
+    publisher = SelectField("Publisher", pubList = [])
+    submit = SubmitField("Add Game")
     
 
 class PublisherForm(FlaskForm):
-    publisher_name = StringField("Publisher")
-    submit = SubmitField("Submit")
+    publisher_name = StringField("Publisher Name")
+    submit = SubmitField("Add Publisher")
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,53 @@
 from application import app, db
 from application.models import Games, Publishers
-from application.forms import TaskForm
+from application.forms import GameForm, PublisherForm
 from flask import redirect, url_for, render_template, request
+
+@app.route('/')
+@app.route('/home')
+@app.route('/indexgames')
+def indexgames():
+    game = Games.query.all()
+    return render_template("game.html", Games = game)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@approute('/indexpublisher')
+def indexpublisher:
+    publisher = Publishers.query.all()
+    return render_template("publisher.html", Publishers = publisher)
+
+@app.route('/addgame', methods=['GET','POST'])
+def addgame():
+    form = GameForm()
+    form.publisher.pubList = [(publishers.id,publishers.publisher_name) for publishers in Publishers.query.all()]
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            gameData = Games(
+                game_name = form.game_name.data,
+                genre = form.genre.data
+                release_date = form.release_date.data
+                price = form.price.data
+                publisher_ID = form.publisher.data
+
+            )
+            db.session.add(gameData)
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('addgame.html', form=form)
+
+@app.route('/addpublisher', methods=['GET', 'POST'])
+def addpublisher():
+    form = PublisherForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            publisherData = Publisher(
+                publisher_name = form.publisher_name.data
+            )
+            db.session.add(publisherData)
+            db.session.commit()
+        return redirect(url_for('indexpublisher'))
+    return render_template('addpublisher.html', form=form)
 
