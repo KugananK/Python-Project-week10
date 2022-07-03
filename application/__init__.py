4d6f7ee2dc6a1c0630552269e410a8d56a72749e
Kajan <kajankugananthajothy@gmail.com>
2022-07-03 22:32:53 +0000

    cloud db

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SECRET_KEY'] = "shhh it's a secret"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
